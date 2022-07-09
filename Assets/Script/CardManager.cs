5d2b10249121039738682617fccc001c305db10b
H2-yeon <87529654+H2-yeon@users.noreply.github.com>
2022-07-09 23:49:02 +0900

    Practice Game

---
+++ Assets/Script/CardManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardManager : MonoBehaviour
+{
+    public static CardManager Inst {get; private set;}
+    void Awake() => Inst = this;
+
+    [SerializeField] ItemSO itemSO;
+
+    List<Item> itemBuffer;
+
+    public Item PopItem(){
+        if (itemBuffer.Count == 0)
+            SetupItemBuffer();
+        Item item = itemBuffer[0];
+        itemBuffer.RemoveAt(0);
+        return item;
+    }
+
+    void SetupItemBuffer() {
+        itemBuffer = new List<Item>();
+        for(int i=0; i<itemSO.items.Length; i++){
+            Item item = itemSO.items[i];
+            itemBuffer.Add(item);
+        }
+        
+        for(int i=0; i<itemBuffer.Count; i++){
+            int rand = Random.Range(i, itemBuffer.Count);
+            Item temp = itemBuffer[i];
+            itemBuffer[i] = itemBuffer[rand];
+            itemBuffer[rand] = temp;
+        }
+    }
+
+    void Start()
+    {
+        SetupItemBuffer();
+    }
+
+    void Update(){
+        if(Input.GetKeyDown(KeyCode.Keypad1))
+            print(PopItem().name);
+    }
+}
