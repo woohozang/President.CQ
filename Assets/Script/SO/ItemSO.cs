5307032c4379336ed09934e78a4880f7ee580e70
SU_VIN <pyotnqls25@naver.com>
2022-07-14 14:26:13 +0900

    그적

--- Assets/Script/SO/ItemSO.cs
+++ Assets/Script/SO/ItemSO.cs
@@ -9,7 +9,10 @@ public class Item
     public Sprite sprite;
 }
 
+//파일 이름이 ItemSo 이고 Scriptable Object/ItemSO 이 폴더에서 생성 가능 
 [CreateAssetMenu(fileName = "ItemSO", menuName = "Scriptable Object/ItemSO")]
+
+
 public class ItemSO : ScriptableObject
 {
     public Item[] items;
