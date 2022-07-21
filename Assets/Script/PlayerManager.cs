c51db1c0897fc190c28da451576d2d542b93571f
SU_VIN <pyotnqls25@naver.com>
2022-07-21 22:14:28 +0900

    그적

--- Assets/Script/PlayerManager.cs
+++ Assets/Script/PlayerManager.cs
@@ -1,19 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class UserManager : MonoBehaviour
 {
-    [SerializeField] List<Item> MyCards;
+    [SerializeField] List<Item> cards;
+    public PhotonView PV;
+    public DatabaseManager db;
     // Start is called before the first frame update
     void Start()
     {
+        db = GameObject.Find("UserInfo").GetComponent<DatabaseManager>();
         
+   
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+
+    void SetCards(List<Item> _cards)
+    {
+        cards = _cards;
     }
 }
